28077ee4012873a4a1ddb5b548660f73a5589a4c
AlekseyH <ahaytman@gmail.com>
2016-07-12 13:39:42 +0300

    Project create/getlist were completed

--- modules/projects/client/controllers/list.projects.client.controller.js
+++ modules/projects/client/controllers/list.projects.client.controller.js
@@ -9,10 +9,26 @@
 
   function ProjectListController(ProjectServices) {
     var vm = this;
-    vm.numberOfPages = 10;
-    vm.amout = 0;
+    vm.numberOfPages = 0;
+    vm.amount = 0;
+    vm.limitedSize = 7;
+    vm.current = 0;
+    vm.array = [];
+
     vm.projects = ProjectServices.query(function (data) {
-      vm.amout = data.length;
+      vm.amount = data.length;
+      vm.numberOfPages = Math.ceil(vm.amount / vm.limitedSize) - 1;
+      vm.array = data;
     });
+
+    vm.getNext = function (input) {
+      vm.current += 1;
+      vm.array = vm.projects.slice(input);
+    };
+
+    vm.getPrevious = function (input) {
+      vm.current -= 1;
+      vm.array = vm.projects.slice(input);
+    };
   }
 }());
--- modules/projects/server/controllers/projects.server.controller.js
+++ modules/projects/server/controllers/projects.server.controller.js
@@ -72,7 +72,7 @@ exports.delete = function (req, res) {
 
 
 exports.list = function (req, res) {
-  Project.find().sort('-created').populate('user, displayName').exec(function (err, projects) {
+  Project.find().sort('created').populate('user, displayName').exec(function (err, projects) {
     if (err) {
       return res.status(400).send({
         message: errorHandler.getErrorMessage(err)
